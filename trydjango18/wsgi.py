53b83475e31b7ef282a2cbef9d4936aa4cdfa889
gowa66 <gowa66at@gmail.com>
2015-09-29 16:16:08 +0300

    my django app

--- trydjango18/wsgi.py
+++ trydjango18/wsgi.py
@@ -16,7 +16,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trydjango18.settings")
 application = get_wsgi_application()
 
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+try:
+	from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+	application = Cling(get_wsgi_application())
+except:
+	pass
+
+	
