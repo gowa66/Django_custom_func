e0f0e88e2ad2106573056142c18bacfb83088809
gowa66 <gowa66at@gmail.com>
2015-10-02 01:50:21 +0300

    ewe

--- trydjango18/urls.py
+++ trydjango18/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from trydjango18 import views
+
 
 urlpatterns = [
     # Examples:
--- trydjango18/views.py
+++ trydjango18/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+
 
 # Create your views here.
 def about(request):
@@ -18,15 +17,3 @@ def custom_400(request):
 def custom_500(request):
 	return render(request, "500.html")
 
-def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
-
-
-def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
