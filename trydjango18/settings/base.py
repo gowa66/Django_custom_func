b9b8887b6838139a0579ae8fe3ada45f57f5b6f6
gowa66 <gowa66at@gmail.com>
2015-10-04 19:52:25 +0300

    gaben2

--- trydjango18/settings/base.py
+++ trydjango18/settings/base.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_comments',
+    
     #third party apps
     'crispy_forms',
     'registration',
