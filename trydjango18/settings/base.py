1833e46f3d8646067cd65041ad56cb7fda3f22fa
gowa66 <gowa66at@gmail.com>
2015-10-22 18:16:12 +0300

    newgallery14

--- trydjango18/settings/base.py
+++ trydjango18/settings/base.py
@@ -147,8 +147,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
 
 STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR, "static_in_pro", "our_static"),
-    # os.path.join(BASE_DIR, "static_in_env"),
+    os.path.join(BASE_DIR, "static_in_pro", "our_static"),
+    os.path.join(BASE_DIR, "static_in_env"),
     # '/var/www/static/',
 )
 MEDIA_URL = '/media/'
